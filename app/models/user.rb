5f68f27aa88eb9f29245a5994de19a29c1e801d2
yusaku0609 <127006537+yusaku0609@users.noreply.github.com>
2023-06-17 21:58:55 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :email,    uniqueness: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
 end
