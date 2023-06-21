e3257288e58979848ec0e2979b364fea62cfeb8c
yusaku0609 <127006537+yusaku0609@users.noreply.github.com>
2023-06-21 21:59:33 +0900

    商品出品機能の修正３

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,8 +27,7 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :shipping_date_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },
-    format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer:true}
   end
 
   # ジャンルの選択が「--」の時は保存不可
